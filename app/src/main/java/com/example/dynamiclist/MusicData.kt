0cefd32fe9663b8bb5f990f7b60e1cf473a24657
Amanda Bressam <amandabressam@gmail.com>
2022-01-31 18:20:47 -0300

    Created MusicData class

---
+++ app/src/main/java/com/example/dynamiclist/MusicData.kt
@@ -0,0 +1,46 @@
+package com.example.dynamiclist
+
+class MusicData {
+    fun loadSongs(): List<Music> = listOf (
+        Music(
+            name = "Bad Habits",
+            time = "03:53"
+        ),
+        Music(
+            name = "Shape Of You",
+            time = "03:55"
+        ),
+        Music(
+            name = "Galway Girl",
+            time = "02:53"
+        ),
+        Music(
+            name = "Faster Car",
+            time = "03:48"
+        ),
+        Music(
+            name = "Let It Be",
+            time = "04:04"
+        ),
+        Music(
+            name = "Here Comes The Sun",
+            time = "03:12"
+        ),
+        Music(
+            name = "Castle On The Hill",
+            time = "04:20"
+        ),
+        Music(
+            name = "Bad Habits",
+            time = "02:58"
+        ),
+        Music(
+            name = "Go Your Onw Way",
+            time = "03:07"
+        ),
+        Music(
+            name = "Time After Time",
+            time = "03:41"
+        ),
+    )
+}
