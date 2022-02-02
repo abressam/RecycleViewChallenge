80c7d5564635fba1801116185ac99ad9b6d357a7
Amanda Bressam <amandabressam@gmail.com>
2022-02-01 21:57:15 -0300

    Updated MusicAdapter class, created function updateList

--- app/src/main/java/com/example/dynamiclist/MusicAdapter.kt
+++ app/src/main/java/com/example/dynamiclist/MusicAdapter.kt
@@ -6,7 +6,10 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class MusicAdapter (private val playlist: List<Music>) : RecyclerView.Adapter<MusicViewHolder>() {
+class MusicAdapter : RecyclerView.Adapter<MusicViewHolder>() {
+
+    private lateinit var playlist: List<Music>
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MusicViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.playlist_music, parent, false)
@@ -19,13 +22,19 @@ class MusicAdapter (private val playlist: List<Music>) : RecyclerView.Adapter<Mu
     }
 
     override fun getItemCount(): Int = playlist.size
-}
 
-class MusicViewHolder (private val view: View) : RecyclerView.ViewHolder(view) {
-    fun bind (music: Music) {
-        view.apply {
-            findViewById<TextView>(R.id.music_name).text = music.name
-            findViewById<TextView>(R.id.music_time).text = music.time
-        }
+    fun updateList(playlist: List<Music>) {
+        this.playlist = playlist
     }
 }
+
+class MusicViewHolder(
+    private val view: View
+    ) : RecyclerView.ViewHolder(view) {
+        fun bind (song: Music) {
+            view.apply {
+                findViewById<TextView>(R.id.music_name).text = song.name
+                findViewById<TextView>(R.id.music_time).text = song.time
+            }
+        }
+    }
