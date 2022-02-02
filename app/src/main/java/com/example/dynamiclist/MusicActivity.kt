a51cba60bec5b2bb6094eb97cfc64743bd463f38
Amanda Bressam <amandabressam@gmail.com>
2022-02-01 21:55:38 -0300

    Updated MusicActivity

--- app/src/main/java/com/example/dynamiclist/MusicActivity.kt
+++ app/src/main/java/com/example/dynamiclist/MusicActivity.kt
@@ -2,19 +2,31 @@ package com.example.dynamiclist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MusicActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_music)
 
         val recyclerView = findViewById<RecyclerView>(R.id.recycle_view)
-        val adapter = MusicAdapter(MusicData().loadSongs())
+
+        val adapter = MusicAdapter()
+
+        val viewModel = ViewModelProvider(this).get(PlaylistViewModel::class.java)
+        viewModel.playlistInfo.observe(this, { playlistInfo ->
+            adapter.updateList(playlistInfo)
+        })
+
         val layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
 
         recyclerView.layoutManager = layoutManager
         recyclerView.adapter = adapter
+
+        viewModel.loadPlaylist()
+
     }
 }
