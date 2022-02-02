dc15c5ad83da1d54b71f3fea767ebb02f57a5202
Amanda Bressam <amandabressam@gmail.com>
2022-02-01 21:55:58 -0300

    Created ViewModel class

--- app/src/main/java/com/example/dynamiclist/PlaylistViewModel.kt
+++ app/src/main/java/com/example/dynamiclist/PlaylistViewModel.kt
@@ -1,4 +1,14 @@
 package com.example.dynamiclist
 
-class PlaylistViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class PlaylistViewModel() : ViewModel () {
+    private val _playlistInfo: MutableLiveData<List<Music>> = MutableLiveData()
+    val playlistInfo: LiveData<List<Music>> = _playlistInfo
+
+    fun loadPlaylist () {
+        _playlistInfo.value = MusicData().loadSongs()
+    }
 }
