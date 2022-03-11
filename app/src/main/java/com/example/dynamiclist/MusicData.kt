97723860718c972d3049a6f50a525014112c189a
Amanda Bressam <amandabressam@gmail.com>
2022-03-11 10:54:08 -0300

    Updated music data

--- app/src/main/java/com/example/dynamiclist/MusicData.kt
+++ app/src/main/java/com/example/dynamiclist/MusicData.kt
@@ -1,46 +1,62 @@
 package com.example.dynamiclist
 
+import android.content.res.Resources
+
 class MusicData {
-    fun loadSongs(): List<Music> = listOf (
+    fun loadSongs(resources: Resources): List<Music> = listOf (
         Music(
-            name = "Bad Habits",
-            time = "03:53"
+            name = resources.getString(R.string.bad_habits),
+            time = resources.getString(R.string.bad_habits_time),
+            image = R.drawable.bad_habits_image,
+            description = resources.getString(R.string.bad_habits_description)
         ),
         Music(
-            name = "Shape Of You",
-            time = "03:55"
+            name = resources.getString(R.string.shape_of_you),
+            time = resources.getString(R.string.shape_of_you_time),
+            image = R.drawable.shape_of_you_image,
+            description = resources.getString(R.string.shape_of_you_description)
         ),
         Music(
-            name = "Galway Girl",
-            time = "02:53"
+            name = resources.getString(R.string.galway_girl),
+            time = resources.getString(R.string.galway_girl_time),
+            image = R.drawable.galway_girl_image,
+            description = resources.getString(R.string.galway_girl_description)
         ),
         Music(
-            name = "Faster Car",
-            time = "03:48"
+            name = resources.getString(R.string.faster_car),
+            time = resources.getString(R.string.faster_car_time),
+            image = R.drawable.faster_car_image,
+            description = resources.getString(R.string.faster_car_description)
         ),
         Music(
-            name = "Let It Be",
-            time = "04:04"
+            name = resources.getString(R.string.let_it_be),
+            time = resources.getString(R.string.let_it_be_time),
+            image = R.drawable.let_it_be_image,
+            description = resources.getString(R.string.let_it_be_description)
         ),
         Music(
-            name = "Here Comes The Sun",
-            time = "03:12"
+            name = resources.getString(R.string.here_comes_the_sun),
+            time = resources.getString(R.string.here_comes_the_sun_time),
+            image = R.drawable.here_comes_the_sum_image,
+            description = resources.getString(R.string.here_comes_the_sun_description)
         ),
         Music(
-            name = "Castle On The Hill",
-            time = "04:20"
+            name = resources.getString(R.string.castle_on_the_hill),
+            time = resources.getString(R.string.castle_on_the_hill_time),
+            image = R.drawable.castle_on_the_hill_image,
+            description = resources.getString(R.string.castle_on_the_hill_description)
         ),
         Music(
-            name = "Bad Habits",
-            time = "02:58"
+            name = resources.getString(R.string.go_your_on_way),
+            time = resources.getString(R.string.go_your_on_way_time),
+            image = R.drawable.go_your_own_way_image,
+            description = resources.getString(R.string.go_your_on_way_description)
         ),
         Music(
-            name = "Go Your Onw Way",
-            time = "03:07"
-        ),
-        Music(
-            name = "Time After Time",
-            time = "03:41"
+            name = resources.getString(R.string.time_after_time),
+            time = resources.getString(R.string.time_after_time_time),
+            image = R.drawable.time_after_time_image,
+            description = resources.getString(R.string.time_after_time_description)
         ),
     )
 }
